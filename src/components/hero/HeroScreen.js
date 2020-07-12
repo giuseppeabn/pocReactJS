bf9b0018a755cf1971468e0b16dde284367ec57f
Giusepppe <giuseppe.varas@bovinoapp.com>
2020-07-12 02:10:17 -0400

    se agregan estilos a las imagenes

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -27,11 +27,11 @@ export const HeroScreen = ({history}) => {
       <div className="col-4">
         <img
           src={`../../../assets/heroes/${heroeId}.jpg`}
-          className="img-thumbnail"
+          className="img-thumbnail animate__animated animate__fadeInLeft"
           alt={superhero}
         />
       </div>
-      <div className="col-8">
+      <div className="col-8 animate__animated animate__fadeIn">
         <h3>{superhero}</h3>
         <ul className="list-group list-group-flush">
           <li className="list-group-item">
